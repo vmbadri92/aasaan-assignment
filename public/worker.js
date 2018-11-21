acf6e6113e5b4c38c313a5f653ca43978a289fc8
code_atlas <vmbadri92@gmail.com>
2018-11-21 19:06:49 +0530

    doCache true for deployment

--- public/worker.js
+++ public/worker.js
@@ -1,5 +1,5 @@
 // Flag for enabling cache in production
-var doCache = false;
+var doCache = true;
 var CACHE_NAME = 'aasaan-assignment';
 // Delete old caches
 self.addEventListener('activate', event => {
